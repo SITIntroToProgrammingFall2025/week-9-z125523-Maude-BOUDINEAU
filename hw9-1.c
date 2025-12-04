a43002f23e6e25dc56933a2ad12dfcf1feaf5e7c
z125523-Maude-BOUDINEAU <z125523@shibaura-it.ac.jp>
2025-12-04 08:22:09 +0100

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -10,14 +10,9 @@ int main(){
             scanf("%d",&a[i][j]);
         }
     }
-    printf("You entered\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d ",a[i][j]);
-        }
-        printf("\n");
 }
 det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
-printf("Determinant: %.2f\n", det);
+printf("Determinant is %.6f\n", det);
 return 0;
 }
+
