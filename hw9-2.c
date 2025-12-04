dead7d033ca2a85ddc62fcc8980f8fe64855bf1c
z125523-Maude-BOUDINEAU <z125523@shibaura-it.ac.jp>
2025-12-04 08:18:57 +0100

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,33 @@
 
+
+#include <stdio.h>
+int main(){
+    int a[3][3];
+    int a_flip[3][3];
+    int i, j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("You entered\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    for (i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            a_flip[i][j] = a[i][2-j];
+        }
+    }
+    printf("Output\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",a_flip[i][j]);
+        }
+        printf("\n");
+    }
+return 0;
+}
