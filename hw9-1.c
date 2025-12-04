b6b6bcb15b77d8f7630b5babe878c51279a5a32c
z125523-Maude-BOUDINEAU <z125523@shibaura-it.ac.jp>
2025-12-04 08:19:14 +0100

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,23 @@
 
+
+#include <stdio.h>
+int main(){
+    int a[2][2];
+    int i, j;
+    double det;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("You entered\n");
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+}
+det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+printf("Determinant: %.2f\n", det);
+return 0;
+}
