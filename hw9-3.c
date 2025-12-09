3b04c2d44ddb4eff93fe2a8a8ef8d1b184244d9c
z125523-Maude-BOUDINEAU <z125523@shibaura-it.ac.jp>
2025-12-09 06:52:00 +0100

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,58 +1,80 @@
+/* hw9-3.c */
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
-    int A[2][3];   
-    int B[3][2];   
-    int C[2][2] = {0}; 
+    int a[2][3];
+    int b[3][2];
+    int c[2][2];
+    int i, j, k;
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            scanf("%d", &A[i][j]);
+    printf("Enter first matrix (2x3):\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            scanf("%d", &B[i][j]);
+    printf("Enter second matrix (3x2):\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &b[i][j]);
         }
     }
 
-    printf("The first matrix you entered is\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", A[i][j]);
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            c[i][j] = 0;
         }
-        printf("\n");
     }
 
-    printf("\nThe second matrix you entered is\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("%d ", B[i][j]);
-        }
-        printf("\n");
-    }
-
-   
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 3; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            for (k = 0; k < 3; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
 
-    printf("The multiplication product of matrix A and matrix B :\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("%d ", C[i][j]);
+    printf("The first matrix you entered is:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The second matrix you entered is:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The multiplication product of matrix A and matrix B is:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d ", c[i][j]);
         }
         printf("\n");
     }
 
     return 0;
 }
-
-
-
